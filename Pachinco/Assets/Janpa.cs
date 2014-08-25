e9ca7cf882f18a8fe76ee03ae990ae6b3bc84d55
jjuiddong <jjuiddong@hanmail.net>
2014-08-25 17:46:02 +0900

    update

---
+++ Pachinco/Assets/Janpa.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Janpa : MonoBehaviour {
+
+	public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider obj) {
+		print ("TriggerEnter");
+		Destroy (obj.gameObject);	
+
+		Vector3 pos;
+		pos.x = 0;
+		pos.y = 2;
+		pos.z = 0;
+
+		Instantiate ( ball, pos, Quaternion.Euler(0,0,0) );
+
+		SendMessage ("GameManager.OutBall");
+
+	}
+
+}
