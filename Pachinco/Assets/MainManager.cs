d7543b630a571f6c564dff684e25f82fddb541ab
jjuiddong <jjuiddong@hanmail.net>
2014-08-25 19:32:51 +0900

    update pachinco

---
+++ Pachinco/Assets/MainManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainManager : MonoBehaviour {
+
+	static int score = 0;
+	public GUIText guiText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		guiText.text = score.ToString ();
+	}
+
+	void ScoreUp() {
+		score++;
+		print ("ScoreUp");
+	}
+
+}
