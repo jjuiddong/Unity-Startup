2dfe9b8a5b571bdbbd6645a85402b96dbaa9dcd3
jjuiddong <jjuiddong@hanmail.net>
2014-08-25 17:07:25 +0900

    update pachinco

---
+++ Pachinco/Assets/IntroUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnMouseDown() {
+		print ("MouseDown");
+		Application.LoadLevel ("scene1");
+	}
+
+}
