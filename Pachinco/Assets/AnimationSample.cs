d011c7042e46ad1312cf4329b34a6568b8a87e8d
jjuiddong <jjuiddong@hanmail.net>
2014-09-05 19:14:36 +0900

    update animator study

---
+++ Pachinco/Assets/AnimationSample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSample : MonoBehaviour {
+
+	Animator animator;
+
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent (typeof(Animator)) as Animator;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			animator.Play("walk2");
+		}
+
+	}
+}
