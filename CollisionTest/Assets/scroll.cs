6abbf475c7983e519cfea4c9ba4bd48fab73a676
jjuiddong <jjuiddong@hanmail.net>
2014-09-10 13:43:34 +0900

    update collision test

---
+++ CollisionTest/Assets/scroll.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroll : MonoBehaviour {
+
+	float offset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset += Time.deltaTime * 0.1f;
+		renderer.material.mainTextureOffset = new Vector2 (offset, 0);	
+	}
+}
---
+++ CollisionTest/Assets/wallCollision.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("start wallCollision");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void  OnCollisionEnter2D(Collision2D collision)
+	{
+		print ("collision");
+
+		collision.rigidbody.AddForce(new Vector3(0, 500, 0));
+
+	}
+
+}
+
